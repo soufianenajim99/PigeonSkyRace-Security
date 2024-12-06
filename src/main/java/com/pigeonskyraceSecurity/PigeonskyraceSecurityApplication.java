c74062d53eae22207577055acf9994a3a9af5348
soufianenajim99 <soufianenajim100@gmail.com>
2024-12-06 10:02:43 +0100

    Initial commit

---
+++ src/main/java/com/pigeonskyraceSecurity/PigeonskyraceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pigeonskyraceSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonskyraceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonskyraceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigeonskyraceSecurity/PigeonskyraceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeonskyraceSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonskyraceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
